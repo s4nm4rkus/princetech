8a041b53af18b0d48e3cfdef6c8edb431882cabf
Markus <92071220+s4nm4rkus@users.noreply.github.com>
2024-12-29 18:31:22 +0800

    Add files via upload

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.mainLayout')
+
+@section('content')
+   
+    @vite('resources/assets/css/home.style.css')
+<body>
+    <div class="home-content">
+        Hello World
+    </div>
+</body>
+@endsection
